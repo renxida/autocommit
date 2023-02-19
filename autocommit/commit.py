3fd4179d5290b4e37940bf6c26b2ffef0f077f01
Benjamin Häublein <benjaminhaeublein@gmail.com>
2023-02-19 19:46:58 +0100

    only get one suggestion

--- autocommit/commit.py
+++ autocommit/commit.py
@@ -7,10 +7,11 @@ import keyring
 import os
 import re
 import markdown
+import sys
 
 from pydantic import BaseModel
 from langchain.llms import OpenAI
-from langchain.prompts import BasePromptTemplate
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,31 +19,23 @@ load_dotenv()
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 
-class CustomPromptTemplate(BasePromptTemplate, BaseModel):
-    template: str
-
-    def format(self, **kwargs) -> str:
-        c_kwargs = {k: v for k, v in kwargs.items()}
-        return self.template.format(**c_kwargs)
-
-
-prompt = CustomPromptTemplate(
-    input_variables=["diff"],
+prompt = PromptTemplate(
+    input_variables=["diff", "repo_info"],
     template="""
     What follows "-------" is a git diff for a potential commit.
-    Reply with a markdown unordered list of 5 possible, different Git commit messages 
+    Reply with what you think is the best possible Git commit message 
     (a Git commit message should be concise but also try to describe 
-    the important changes in the commit), order the list by what you think 
-    would be the best commit message first, and don't include any other text 
-    but the 5 messages in your response.
+    the important changes in the commit). Print exactly the proposed 
+    commit message and not a single word more, no introduction text. 
+    The git repository is described as: 
+    {repo_info}
     ------- 
     {diff}
-    -------
 """,
 )
 
 
-def generate_suggestions(diff, openai_api_key=OPENAI_KEY):
+def generate_suggestion(diff, repo_info, openai_api_key=OPENAI_KEY):
 
     llm = OpenAI(
         temperature=0.2,
@@ -52,15 +45,10 @@ def generate_suggestions(diff, openai_api_key=OPENAI_KEY):
     )  # type: ignore
 
     # query OpenAI
-    formattedPrompt = prompt.format(diff=diff)
+    formattedPrompt = prompt.format(diff=diff, repo_info=repo_info)
     response = llm(formattedPrompt)
 
-    # Convert the markdown string to HTML
-    html = markdown.markdown(response)
-
-    # Use a regular expression to extract the list items from the HTML
-    items = re.findall(r"<li>(.*?)</li>", html)
-    return items
+    return response
 
 
 SERVICE_ID = "auto-commit-cli"
@@ -82,31 +70,18 @@ def prompt_for_openai_api_key():
 
 def main():
     # Prompt for OpenAI API key if it's not set
-    openai_api_key = keyring.get_password(SERVICE_ID, "user")
-    if openai_api_key is None:
-        openai_api_key = prompt_for_openai_api_key()
+    if OPENAI_KEY:
+        openai_api_key = OPENAI_KEY
+    else:
+        openai_api_key = keyring.get_password(SERVICE_ID, "user")
+        if openai_api_key is None:
+            openai_api_key = prompt_for_openai_api_key()
+    diff = sys.stdin.read()
 
-    #  Get the diff including untracked files (see https://stackoverflow.com/a/52093887)
-    git_command = "git --no-pager diff; for next in $( git ls-files --others --exclude-standard ) ; do git --no-pager diff --no-index /dev/null $next; done;"
-
-    # Windows support
-    if os.name == "nt":
-        git_command = """git diff && for /f "delims=" %a in ('git ls-files --others --exclude-standard') do (git diff --no-index /dev/null %a)"""
-
-    output = subprocess.run(
-        git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-
-    if len(output.stderr) > 0:
-        error = output.stderr.decode("utf-8")
-        print("There was an error retrieving the current diff: ")
-        if error.startswith("warning: Not a git repository"):
-            print("You're not inside a git repo. Please run it from inside a git repo.")
-        else:
-            print(error)
+    if len(diff) == 0:
+        print("There was an error retrieving the current diff")
         exit(-1)
 
-    diff = output.stdout.decode("utf8")
     # Trim the diff
     diff = diff.strip()
 
@@ -114,10 +89,12 @@ def main():
         print("Diff is empty. Nothing to commit.")
         exit(0)
 
-    suggestions = []
+    repo_info = sys.argv[1]
+
+    suggestion = None
 
     try:
-        suggestions = generate_suggestions(diff[:7000], openai_api_key=openai_api_key)
+        suggestion = generate_suggestion(diff[:7000], repo_info, openai_api_key=openai_api_key)
     except Exception as e:
         print("There was an error generating suggestions from OpenAI: ")
         # Prompt for OpenAI API key if it's incorrect
@@ -127,67 +104,7 @@ def main():
         else:
             print(e)
         exit(-1)
-
-    if len(suggestions) == 0:
-        print("No suggestions found.")
-        exit(0)
-
-    # Prompt user with commit messages and choices and allow edits
-    custom_style = style_from_dict(
-        {
-            Token.Separator: "#6C6C6C",
-            Token.QuestionMark: "#000000",
-            Token.Selected: "#FFFF00 bold",
-            Token.Pointer: "#FF9D00 bold",
-            Token.Instruction: "",
-            Token.Answer: "#EA9104 bold",
-            Token.Question: "",
-        }
-    )
-
-    questions = [
-        {
-            "type": "list",
-            "name": "commit_message",
-            "message": "Commit message suggestions:",
-            "choices": [f"{i + 1}. {item}" for i, item in enumerate(suggestions)],
-            "filter": lambda val: val[3:],
-        }
-    ]
-    answers = py_inquirer_prompt(questions, style=custom_style)
-    answers = py_inquirer_prompt(
-        [
-            {
-                "type": "input",
-                "name": "final_commit_message",
-                "message": "Confirm or edit the commit message:",
-                "default": answers.get("commit_message"),
-            },
-        ]
-    )
-
-    # Commit the changes
-    git_command = (
-        'git add -A; git commit -m "' + answers.get("final_commit_message") + '"'
-    )
-
-    # Windows support
-    if os.name == "nt":
-        git_command = (
-            'git add -A && git commit -m "' + answers.get("final_commit_message") + '"'
-        )
-
-    output = subprocess.run(
-        git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-
-    if len(output.stderr) > 0:
-        error = output.stderr.decode("utf-8")
-        print("There was an error committing:")
-        print(error)
-    else:
-        print("Commit successful with message:", answers.get("final_commit_message"))
-
+    print(suggestion)
 
 if __name__ == "__main__":
     main()
